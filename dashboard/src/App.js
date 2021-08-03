9937ff8b8857731e5040b0dc2eaf1e0f91f01673
Dreed657 <ninjakiviWTF@gmail.com>
2021-08-03 16:30:00 +0300

    Add dashboard to docker-compose

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
 import Dashboard from './components/Dashboard';
 import Themes from './themes';
-import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
